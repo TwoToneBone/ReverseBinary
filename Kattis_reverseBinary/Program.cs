601bc50364847cdb5a0b58665cd9a12643c396ba
Erik <erik.ra@gmail.com>
2017-04-06 22:31:50 +0200

    Add project files.

---
+++ Kattis_reverseBinary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Kattis_ReverseBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(Convert.ToUInt32(new string(Convert.ToString(uint.Parse(Console.ReadLine()), 2).Reverse().ToArray()), 2));
+            }
+            catch (Exception e)
+            {
+
+                Debug.WriteLine(e.Message);
+            }               
+        }
+    }
+}
